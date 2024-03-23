9d1398485c4122174f78938a601328e2ed63fbe0
abhii718 <abhinaysingh47639@gamil.com>
2024-03-23 11:02:58 +0530

    frontend and backend by chai with code

--- chaiwithcode/backend/server.js
+++ chaiwithcode/backend/server.js
@@ -1,13 +1,9 @@
 import express from "express";
 const app = express();
 
-// app.get("/", (req, res) => {
-//   res.send("Server is ready");
-// });
-
 //get a list of 5 jokes
 
-app.get("/jokes", (req, res) => {
+app.get("/api/jokes", (req, res) => {
   const jokes = [
     {
       id: 1,
--- chaiwithcode/frontend/src/App.jsx
+++ chaiwithcode/frontend/src/App.jsx
@@ -1,26 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios from "axios";
 import './App.css'
 
 function App() {
-  const [jokes, setjokes] = useState([])
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+      .then((response) => {
+        setJokes(response.data);
+      }).catch((error) => {
+        console.log("Error", error);
+      });
+  }, []);
 
   return (
     <>
-      <h1>hello</h1>
+      <h1>Hello</h1>
       <p>Jokes: {jokes.length}</p>
 
-      {
-        jokes.map((joke, index) => {
-          <div key={joke.id}>
-            <h3>{joke.title}</h3>
-            <p>{joke.content}</p>
-          </div>
-        })
+      {jokes.map((joke, index) => (
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>
+      ))
       }
     </>
-  )
+  );
 }
 
-export default App
+export default App;
