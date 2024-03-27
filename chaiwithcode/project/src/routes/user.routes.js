70aa5397f124ba22dca310148d7b33801730b63e
abhii718 <abhinaysingh47639@gamil.com>
2024-03-27 22:18:19 +0530

    "Add user routes for changing password, getting current user, updating account details, avatar, cover image, channel profile, and watch history"
    This commit adds the following changes to the user routes:
    
    * Adds a new route for changing the current user's password with the `changeCurrentPassword` controller.
    * Adds a new route for getting the current authenticated user with the `getCurrentUser` controller.
    * Adds a new route for updating the account details of the current user with the `updateAccountDetail` controller.
    * Adds a new route for updating the avatar of the current user with the `updateUserAvatar` controller.
    * Adds a new route for updating the cover image of the current user with the `updateUserCoverImage` controller.
    * Adds a new route for getting the channel profile of a user with the `getUserChannelProfile` controller.
    * Adds a new route for getting the watch history of the current user with the `getWatchHistory` controller.
    
    All of these routes are secured with the `verifyJWT` middleware to ensure that only authenticated users can access them.

--- chaiwithcode/project/src/routes/user.routes.js
+++ chaiwithcode/project/src/routes/user.routes.js
@@ -4,9 +4,17 @@ import {
   registerUser,
   loginUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetail,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 
 const router = Router();
 router.route("/register").post(
@@ -28,5 +36,13 @@ router.route("/login").post(loginUser);
 //secure  routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetail)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage") , updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
+
 
 export default router;
