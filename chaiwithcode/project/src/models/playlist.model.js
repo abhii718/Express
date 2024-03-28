1873f94f7a96c508aea31c2eec886d64f454188a
abhii718 <abhinaysingh47639@gamil.com>
2024-03-28 12:08:49 +0530

    "Add Playlist model with videos and owner references"
    This commit adds a new file for the Playlist model, which defines a Mongoose schema for playlists with properties for name, description, an array of video references, and a reference to the playlist's owner. The timestamps option is also set to track creation and update times.

---
+++ chaiwithcode/project/src/models/playlist.model.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const playlistSchema = new Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    description: {
+      type: String,
+      require: true,
+    },
+    videos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
